f942cb8ef9383768f0652968c9e57cf2a53313de
Saif Ul Islam <saifulislam84210@gmail.com>
2021-10-01 21:08:27 +0500

    Naive socket.io implementation
    Signed-off-by: Saif Ul Islam <saifulislam84210@gmail.com>

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 var createError = require('http-errors');
 var express = require('express');
+const http = require('http');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
@@ -8,6 +9,10 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+const server = http.createServer(app);
+const { Server } = require('socket.io');
+const io = new Server(server);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -22,6 +27,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/editor', express.static(path.join(__dirname, 'app')));
+
+io.on('connection', socket => {
+  console.log('a user connected');
+});
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
