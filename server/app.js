28fad231f71211c4db0b4edfe17d2b50cbe715ef
Saif Ul Islam <saifulislam84210@gmail.com>
2021-10-03 14:14:48 +0500

    User disconnection
    Signed-off-by: Saif Ul Islam <saifulislam84210@gmail.com>

--- server/app.js
+++ server/app.js
@@ -3,12 +3,28 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+var countUserPresence = 0;
 
 app.use('/static', express.static('public'));
 app.use('/node_modules', express.static('node_modules'));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'views/index.html'));
+});
+
+io.on("connection", (socket) => {
+  console.log("A user connected");
+  countUserPresence += 1;
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+    countUserPresence -= 1;
+
+    if (countUserPresence == 0) {
+      console.log('Zero users present');
+    }
+  });
 });
 
 server.listen(3000, () => {
